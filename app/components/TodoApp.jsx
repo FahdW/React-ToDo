2a7c8db75ab673c1da793325e00db4febaa65df2
Fahd <fwahab9@gmail.com>
2017-02-20 14:26:40 -0800

    add todo list

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import TodoList from 'TodoList';
 
 export default class TodoApp extends React.Component {
-  render() {
+  constructor (props) {
+    super(props);
+    this.state = {
+      todos: [
+        {
+          id: 1,
+          text: 'Walk the dog'
+        },
+        {
+          id: 2,
+          text: 'Eat some food'
+        },
+        {
+          id: 3,
+          text: 'Another one'
+        },
+        {
+          id: 4,
+          text: 'Clean house'
+        }
+      ]
+    }
+  }
+  render () {
+    let {todos} = this.state;
     return(
       <div>
-        TodoApp.jsx
+        <TodoList todos={todos} />
       </div>
     );
   }
