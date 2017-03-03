ab070c18aea8ca4243d48f24b93cbc8ed609b2ae
Fahd <fwahab9@gmail.com>
2017-03-02 16:11:56 -0800

    add reducers and tests

---
+++ app/tests/reducers/reducers.test.jsx
@@ -0,0 +1,28 @@
+import expect from 'expect';
+var df = require('deep-freeze-strict');
+var reducers = require('reducers');
+
+describe('Reducers', () => {
+  describe('searchTextReducer', () => {
+    it('should set searchText', () => {
+      var action = {
+        type: 'SET_SEARCH_TEXT',
+        searchText: 'Test searchText'
+      };
+      var res = reducers.searchTextReducer(df(''), df(action));
+
+      expect(res).toEqual(action.searchText);
+    });
+  });
+
+  describe('showCompeletedReducer', () => {
+    it('should toggle showCompleted', () => {
+      var action = {
+        type: 'TOGGLE_SHOW_COMPLETED'
+      };
+      var res = reducers.showCompletedReducer(df(false), df(action));
+
+      expect(res).toEqual(true);
+    });
+  });
+});
